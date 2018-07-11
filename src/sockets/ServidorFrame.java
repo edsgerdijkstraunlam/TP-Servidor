e835058a5f43675881ac1bbfb49d9b007cebb6bc
Franco David Blanco <riddid117@hotmail.com>
2018-07-11 00:16:37 -0300

    Se implementa el uso de la bibliotecta GSON para el envio de mensajes cliente-servidor. Se arregla error con lista de usuarios conectados

--- src/sockets/ServidorFrame.java
+++ src/sockets/ServidorFrame.java
@@ -1,18 +1,24 @@
 package sockets;
 
+import java.awt.Dimension;
+import java.awt.event.ComponentEvent;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 import dijkstra_paqueteEnvio.PaqueteEnvio;
+
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+
+import com.google.gson.Gson;
+
 import javax.swing.JTextArea;
 
 public class ServidorFrame extends JFrame implements Runnable {
@@ -23,26 +29,21 @@ public class ServidorFrame extends JFrame implements Runnable {
 	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
-	
-	ObjectInputStream entrada;
-	ServerSocket server; // Prepara un puente para que se conecten otros sockets
-	Socket socket; // Prepara un puente para conectarse a un serverSocket
-	int puerto = 9998; // El puerto por el que acceden los datos
-	int puerto2 = 9996;
-	int puertoParaConexionesActivas = 9990;
-	DataOutputStream salida;// Flujo de datos para conectar con el puente de salida y enviar el mensaje
-	ArrayList<PaqueteEnvio> listaDeSockets;
-	ArrayList<Integer> tiempoDeSockets;
-	JComboBox<String> cb;
+	private DataInputStream entrada;
+//	private ObjectInputStream entrada;
+	private ServerSocket server; // Prepara un puente para que se conecten otros sockets
+	private Socket socket; // Prepara un puente para conectarse a un serverSocket
+	private int puertoClienteAServidor = 9998; // El puerto por el que acceden los datos
+	private int puertoServidorACliente = 9996;
+	private int puertoParaConexionesActivas = 9994;
+	private ArrayList<PaqueteEnvio> listaDeSockets;
+	private ArrayList<Integer> tiempoDeSockets;
+	private JComboBox<String> cb;
 	private JTextArea textArea;
 
-	boolean hilo1Work = false;
+	private boolean hilo1Work = false;
 
-	boolean hilo2Work = false;
-
-	public void setTextArea(String text) {
-		this.textArea.setText(text);
-	}
+	private boolean hilo2Work = false;
 
 	public ServidorFrame() {
 		listaDeSockets = new ArrayList<PaqueteEnvio>();
@@ -52,15 +53,16 @@ public class ServidorFrame extends JFrame implements Runnable {
 
 			public void run() {
 
+				Gson gson= new Gson();
 				while (true) {
 					try {
 						hilo1Work = false;
 						ServerSocket ser = new ServerSocket(puertoParaConexionesActivas);
 						Socket conex = ser.accept();
-						ObjectInputStream im = new ObjectInputStream(conex.getInputStream());
-						
-						PaqueteEnvio paq=(PaqueteEnvio) im.readObject();
-						//String nick = im.readUTF();
+						DataInputStream in = new DataInputStream(conex.getInputStream());
+						String json= in.readUTF();
+						PaqueteEnvio paq = gson.fromJson(json, PaqueteEnvio.class);
+						// String nick = im.readUTF();
 						while (hilo2Work)
 							Thread.sleep(200);
 						hilo1Work = true;
@@ -71,21 +73,21 @@ public class ServidorFrame extends JFrame implements Runnable {
 							if (sock.getNick().equals(paq.getNick())) {
 								find = true;
 								tiempoDeSockets.set(i, 3);
+								break;
 							}
 
 						}
 						if (!find) {
-							
-							textArea.append("\nConectado usuario: " + paq.getNick() + "\n");
-							
+
+							textArea.append("\nConectado usuario: " + paq.getNick() + "---->" + paq.getIp() + "\n");
+
 							listaDeSockets.add(paq);
 							tiempoDeSockets.add(3);
 							cb.addItem(paq.getNick());
 						}
 
 						ser.close();
-					} catch (IOException | InterruptedException | ClassNotFoundException e2) {
-						// TODO Auto-generated catch block
+					} catch (IOException | InterruptedException  e2) {
 						e2.printStackTrace();
 					}
 
@@ -119,7 +121,6 @@ public class ServidorFrame extends JFrame implements Runnable {
 
 						}
 					} catch (InterruptedException e2) {
-						// TODO Auto-generated catch block
 						e2.printStackTrace();
 					}
 
@@ -139,8 +140,26 @@ public class ServidorFrame extends JFrame implements Runnable {
 		contentPane.add(cb);
 
 		textArea = new JTextArea();
-		textArea.setBounds(10, 60, 424, 250);
+		textArea.setBounds(10, 60, this.getWidth() - 40, this.getHeight() - 115);
 		contentPane.add(textArea);
+		JScrollPane scroll = new JScrollPane(textArea);
+		scroll.setBounds(10, 60, this.getWidth() - 40, this.getHeight() - 115);
+		this.add(scroll);
+
+		JFrame f = this;
+		this.addComponentListener(new java.awt.event.ComponentAdapter() {
+			public void componentResized(ComponentEvent e) {
+
+				// Se obtienen las dimensiones en pixels de la pantalla.
+				// Se obtienen las dimensiones en pixels de la ventana.
+				Dimension ventana = f.getSize();
+
+				textArea.setSize(ventana.width - 40, ventana.height - 115);
+				scroll.setSize(ventana.width - 40, ventana.height - 115);
+
+			}
+
+		});
 
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
@@ -150,70 +169,60 @@ public class ServidorFrame extends JFrame implements Runnable {
 	public void run() {
 
 		try {
-			server = new ServerSocket(puerto); // Intenta crear el puente del lado del servidor
+			server = new ServerSocket(puertoClienteAServidor);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} // Intenta crear el puente del lado del servidor
 
-			textArea.setText("Esperando Conexion...\n");
+		textArea.setText("Esperando Conexion...\n");
 
-			while (true) {
-				// socket = new Socket();
-
-				socket = server.accept(); // Espera a que alguien se quiera conectar y lo guarda
-				//String direccion = socket.getInetAddress().toString().replace("/", "");
-				//textArea.append("Conectado usuario: " + direccion + "\n");
-				
-
-			
-
-				entrada = new ObjectInputStream(socket.getInputStream());
-			
-				PaqueteEnvio paquete= (PaqueteEnvio)entrada.readObject();
-
-				ArrayList<PaqueteEnvio>users=listaDeSockets;
-				
-				for (PaqueteEnvio usuario : users) {
-					Socket s= new Socket(usuario.getIp(),puerto2);
-					ObjectOutputStream o= new ObjectOutputStream(s.getOutputStream());
-					o.writeObject(paquete);
-					s.close();
-				}
-				
-				
-				textArea.append(paquete.getNick() + "---->" + paquete.getMensaje() + "--" + paquete.getIp()+"\n");
-
-				/*
-				 * DataInputStream recibe = new DataInputStream(socket.getInputStream());
-				 * String[] mensajeCompuesto = recibe.readUTF().split("&"); String ips =
-				 * mensajeCompuesto[0]; String mensaj = mensajeCompuesto[1];
-				 * 
-				 * 1) GetImputStream obtiene el flujo de datos de entrada, que es por donde
-				 * viene el mensaje y lo crea como un nuevo ImputStreamReader 2) BufferReader
-				 * obtiene el mensaje que viaja por el imputStreamReader y lo guarda en entrada
-				 */
-
-				/*
-				 * try { socket = new Socket(ips, puerto2);// Intenta creal un puente con la ip
-				 * a la que se desea // enviar el mensaje } catch (Exception e) {
-				 * System.out.println("IP no valida"); // Si no lo logra Imprime que la ip no es
-				 * valida (lo imprime // solo en la consola asi que es para probar solamente) }
-				 * 
-				 * salida = new DataOutputStream(socket.getOutputStream()); // Crea un flujo de
-				 * salida conectada al // puente con el cliente al que se va a // enviar el
-				 * mensaje salida.writeUTF(mensaj);// Se envia el mensaje
-				 */
-			}
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		} finally {
+		while (true) {
+			// socket = new Socket();
 			try {
 
-				// No estoy muy seguro si esto esta bien. En el bloque finally(asi salte o no
-				// una excepcion), se cierran todos los sockets y los flujos de datos
-				// salida.close();
-				entrada.close();
-				server.close();
-				socket.close();
+				socket = server.accept(); // Espera a que alguien se quiera conectar y lo guarda
+				// String direccion = socket.getInetAddress().toString().replace("/", "");
+				// textArea.append("Conectado usuario: " + direccion + "\n");
+
+				entrada = new DataInputStream(socket.getInputStream());
+
+				Gson gson=new Gson();
+				String json=entrada.readUTF();
+				PaqueteEnvio paquete = gson.fromJson(json, PaqueteEnvio.class);
+
+			//	ArrayList<PaqueteEnvio> users = listaDeSockets;
+
+				ArrayList<PaqueteEnvio> users = new ArrayList<PaqueteEnvio>();
+				users.addAll(listaDeSockets);
+				
+				
+				Socket s;
+				int cant= users.size();
+				for (int i=0;i <cant;i++) {
+					PaqueteEnvio usuario= users.get(i);
+					if (!paquete.getNick().equals(usuario.getNick())) {
+						
+						s = new Socket(usuario.getIp(), puertoServidorACliente);
+						DataOutputStream o = new DataOutputStream(s.getOutputStream());
+						json=gson.toJson(paquete);
+						o.writeUTF(json);
+						s.close();
+					}
+				}
+
+				textArea.append(paquete.getNick() + "---->" + paquete.getMensaje() + "--" + paquete.getIp() + "\n");
+				
 			} catch (IOException e) {
 				e.printStackTrace();
+			} finally {
+
+				try {
+
+					entrada.close();
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 
